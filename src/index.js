acda79fdb65ea0d38f20359966764d0efb5b8576
kidylee <kidylee@gmail.com>
2017-09-18 17:40:54 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { AppContainer } from 'react-hot-loader'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import 'semantic-ui-css/semantic.min.css';
+
+function render(Component) {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App)
+  })
+}
+
+registerServiceWorker()
